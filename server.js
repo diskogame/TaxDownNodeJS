07b67e2aec6b2f5c014b4aefbcd76a8d46ab07e0
Diskoteken <diskogame@gmail.com>
2022-09-28 18:43:21 +0200

    Created CRUD operations.

--- server.js
+++ server.js
@@ -1,56 +1,66 @@
 var express = require('express'); //Node.js web application framework
 var app = express();
 var fs = require("fs");
+var listCustomers;
+
+
+var SEQ_CUSTOMERS = 0;
 
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
-})
-
-app.get('/listUsers', function (req, res) {
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-        console.log( data );
-        res.end( data );
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(data);
+        listCustomers = JSON.parse(data); //Convirte el JSON a un objeto
+        SEQ_CUSTOMERS = Object.keys(listCustomers).length + 1;
     });
 })
 
-var user = {
-    "user4" : {
+app.get('/listCustomers', function (req, res) {
+    console.log(JSON.stringify(listCustomers));
+    res.end(JSON.stringify(listCustomers));
+})
+
+var customer = {
+    "4" : {
         "name" : "mohit",
         "password" : "password4",
         "profession" : "teacher",
-        "id": 4
     }
 }
-
-app.post('/addUser', function (req, res) {
-    // First read existing users.
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-        data = JSON.parse( data );
-        data["user4"] = user["user4"];
-        console.log( data );
-        res.end( JSON.stringify(data));
-    });
+//CREATE
+app.post('/add', function (req, res) {
+    listCustomers[SEQ_CUSTOMERS] = customer["4"];
+    console.log(JSON.stringify(listCustomers));
+    res.end("Customer añadido con exito, id = " + SEQ_CUSTOMERS);
+    SEQ_CUSTOMERS++;
 })
-
+//READ
 app.get('/:id', function (req, res) {
-    // First read existing users.
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-        var users = JSON.parse( data );
-        var user = users["user" + req.params.id]
-        console.log( user );
-        res.end( JSON.stringify(user));
-    });
+    var id = req.params.id;
+    var user = listCustomers[id];
+    console.log(JSON.stringify(user));
+    res.end(JSON.stringify(user));
+
+})
+//UPDATE
+app.put('/update/:id', function (req, res) {
+    var id = req.params.id;
+    listCustomers[id] = customer["4"];
+    console.log(JSON.stringify(listCustomers));
+    res.end("Customer actualizado con exito, id = " + id);
 })
 
-app.delete('/deleteUser', function (req, res) {
-    // First read existing users.
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-        data = JSON.parse( data );
-        delete data["user" + 2];
 
-        console.log( data );
-        res.end( JSON.stringify(data));
-    });
+//DELETE
+app.delete('/delete/:id', function (req, res) {
+    var id = req.params.id;
+    delete listCustomers[id];
+    console.log(JSON.stringify(listCustomers));
+    res.end(JSON.stringify(listCustomers));
 })
